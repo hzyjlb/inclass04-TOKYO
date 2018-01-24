a3149cc11e130eab2099241ca3e3f883140f2413
hzyjlb <blu3@u.rochester.edu>
2018-01-24 16:27:23 -0500

    testing

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<?php
+  	include 'includes/nav.php';
+  	?>
+	<meta charset="utf-8">
+	<title>Home</title>
+</head>
+<body>
+  <p>Hello World!</p>
+
+  
+
+</body>
+</html>
